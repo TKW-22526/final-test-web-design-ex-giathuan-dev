fa14ade40a32c263dfe708f94cb988e1ef6a12f9
Nguyễn Gia Thuận <1112007ngt@gmail.com>
2026-07-15 23:07:18 +0700

    Em Nộp Dự án

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// js/script.js
+const productlistt = [
+    {
+        id: "1",
+        name: "Toán",
+        price: "100k",
+        description: "Luyện tư duy logic",
+        image: "../assets/toan.jpg", 
+    },
+    {
+        id: "2",
+        name: "Ngữ Văn",
+        price: "120k",
+        description: "Văn học hiện đại",
+        image: "../assets/nguvan.jpg",
+    },
+    {
+        id: "3",
+        name: "Tin học",
+        price: "110k",
+        description: "Kỉ nguyên số",
+        image: "../assets/tinhoc.jpg",
+    },
+    {
+        id: "4",
+        name: "Lịch sử",
+        price: "150k",
+        description: "Lịch sử hình thành và phát triển...",
+        image: "../assets/lichsu.jpg",
+    },
+    {
+        id: "5",
+        name: "Sinh học",
+        price: "110k",
+        description: "Lí thuyết về tế bào",
+        image: "../assets/sinhhoc.jpg",
+    },
+    {
+        id: "6",
+        name: "Kĩ thuật lập trình",
+        price: "135k",
+        description: "Cơ sở dữ liệu, thuật toán và cấu trúc dữ liệu căn bản",
+        image: "../assets/kithuatlaptrinh.jpg",
+    },
+    {
+        id: "7",
+        name: "Lập trình hướng đối tượng",
+        price: "145k",
+        description: "Tìm hiểu về các tính chất đóng gói, kế thừa, đa hình",
+        image: "../assets/laptrinh.jpg",
+    },
+    {
+        id: "8",
+        name: "Giải tích A2",
+        price: "125k",
+        description: "Phép tính vi phân, tích phân của hàm nhiều biến",
+        image: "../assets/viticha2.jpg",
+    }
+];
